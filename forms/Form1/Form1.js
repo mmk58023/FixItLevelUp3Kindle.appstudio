cc49614dcbdd4eb4f79e97355e84a58ed37dafec
Maya <mmk58023@creighton.edu>
2020-02-23 04:53:08 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -4,7 +4,7 @@ let userName = prompt('What is your name?')
 while (userName !== null) { 
 
 //Prompts in order to get the loan information
-  let userNeed = prompt(`Which calculator would you like to use, ${userName}? \n Please enter "car" or "home" in all lowercase. \n Warning - A timeframe of 15 to 30 years is required for the calculator to run. \n Enter "stop" when indicated to prompt the calculator to stop once the loop has finished.`)
+  let userNeed = prompt(`Which calculator would you like to use, ${userName}? \n Please enter "car" or "home" in all lowercase. \n Warning - A timeframe of 15 or 30 years is required for the calculator to run. \n Enter "stop" when indicated to prompt the calculator to stop once the loop has finished.`)
 
 //Base function
 function loanPayment (P, r, n) {
@@ -30,7 +30,7 @@ if (userNeed === 'car') {
     alert(`A car loan for ${initialAmount} over ${length} years at ${percent}% interest would have a monthly payment of ${carPayment}.`)
     console.log(`A car loan for ${initialAmount} over ${length} years at ${percent}% interest would have a monthly payment of ${carPayment}.`)
 //If the user enter's "home" calculator rather than "car" with the correct timeframe then the following will occur
-  } else if ((userNeed === 'home') && (length >= 15) && (length <= 30)) {
+  } else if ((userNeed === 'home') && ((length === 15) || (length === 30))) {
 //Calling the function with the user's loan details as the parameters
     let homePayment = loanPayment(initialAmount, interestRate, numberOfMonths)
 //Notification that the monthly payment has been calculated
